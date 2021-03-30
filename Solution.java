55ed26ae06c59c882f7836cefdeb8b3ad6ef3aa8
Abijith Vasanthakumar <42151354+Abjcodes@users.noreply.github.com>
2021-03-30 17:42:45 +0530

    New template with input and output txt files

--- Solution.java
+++ Solution.java
@@ -1,6 +1,3 @@
-/*input
-Test cases
-*/
 import java.io.BufferedReader; 
 import java.io.IOException; 
 import java.io.InputStreamReader; 
@@ -8,10 +5,10 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.math.BigInteger;
 import java.util.*; 
+import java.io.*;
 
-public class Solution
-{
-    static class FastReader 
+public class solution {
+	static class FastReader 
     { 
         BufferedReader br; 
         StringTokenizer st; 
@@ -66,17 +63,42 @@ public class Solution
             } 
             return str; 
         } 
-    } 
-    
-    public static void main(String[] args) {
-        try{
-           FastReader s = new FastReader();
-           StringBuilder sb = new StringBuilder();
-           /*
-           Your code
-           */
-        }catch(Exception e){
-            return;
-        }
     }
+	public static void main(String[] args) {
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+		FastReader s = new FastReader();
+        StringBuilder sb = new StringBuilder();
+		int n = s.nextInt();
+           long[] a = new long[n];
+           for(int i = 0; i<n ; i++)
+           {
+            a[i]= s.nextLong();
+           }
+           Arrays.sort(a);
+           if(a[0] == a[n-1])
+           {
+           sb.append("0" + " " + n*(n-1)/2);
+            }
+            else{
+                sb.append(a[n-1] - a[0]+ " ");
+           long min_count = 0;
+           long max_count = 0;
+           long min = a[0];
+           long max = a[n-1];
+           for(int i = 0; i<n ; i++)
+           {
+            if(a[i] == min)
+                min_count++;
+            else if(a[i] == max)
+                max_count++;
+           }
+           sb.append(min_count*max_count);
+           }
+           System.out.println(sb);
+	}
 }
